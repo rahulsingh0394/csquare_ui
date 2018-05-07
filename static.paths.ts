dadd6ce59e5390433cdb94d32f33308333a27bcf
Rahul Singh <rahul.singh@nmbrsystems.com>
2018-05-07 17:14:09 +0530

    Static path render

--- static.paths.ts
+++ static.paths.ts
@@ -3,5 +3,13 @@ export const ROUTES = [
   '/home',
   '/about',
   '/question',
-  '/contact'
+  '/contact',
+  '/bangalore/home-tuition/home-tuition-in-bangalore',
+  '/bangalore/home-tuition/private-tuition-in-bangalore',
+  '/terms',
+  '/privacy',
+  '/parents-review',
+  '/careers',
+  '/media',
+  '/tutors-review'
 ];
